d91ef588ee8a0f951a777a250c5015b6d4a0e592
789349 <vinay.dubey0900@gmail.com>
2022-07-01 20:06:56 +0530

    added the components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
+import { CardComponent } from './common/card/card.component';
+import { CarouselComponent } from './common/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MovieComponent,
+    CardComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
